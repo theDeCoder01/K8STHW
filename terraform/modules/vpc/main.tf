23cf3d8af5e41b3c852b0fb361a25696aab9067e
Ali Farrah <ali_ma_farrah@hotmail.com>
2026-09-18 22:18:37 +0300

    Create a VPC module draft

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,1 +1,48 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = { Name = var.name }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = { Name = "${var.name}-igw" }
+}
+
+resource "aws_subnet" "public" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.name}-public-${each.key}" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  # the ${var.name}- is used to be able to append the "public-rt" to it.
+  tags = { Name = "${var.name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,1 +1,24 @@
 
+variable "name" {
+  description = "Name prefix for VPC resources."
+  type        = string
+  default     = "project1"
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnets" {
+  description = "Public subnets to create, keyed by a stable name used in the resource address and tags."
+  type = map(object({
+    cidr_block        = string
+    availability_zone = string
+  }))
+
+  validation {
+    condition     = length(var.public_subnets) > 0
+    error_message = "At least one public subnet must be defined."
+  }
+}
