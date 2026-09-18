2aadc5441b8913ca91eb75ccb87bf05a8ce53e16
Ali Farrah <ali_ma_farrah@hotmail.com>
2026-09-19 01:24:01 +0300

    fix a mistake in security group description

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -45,8 +45,8 @@ locals {
 }
 
 resource "aws_security_group" "this" {
-  name        = var.name
-  description = "Security group for ${var.name}"
+  name_prefix = "${var.name}-"
+  description = var.description
   vpc_id      = var.vpc_id
 
   dynamic "ingress" {
